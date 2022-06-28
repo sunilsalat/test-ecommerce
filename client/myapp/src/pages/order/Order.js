89d8d2a6513fa6c2731190237c4d1beed38cc49c
sunilsalat <sunilsalat@yahoo.com>
2022-06-28 09:36:25 +0530

    order page added

--- client/myapp/src/pages/order/Order.js
+++ client/myapp/src/pages/order/Order.js
@@ -1,5 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { placeOrder } from "../../slices/orderSlice";
+
 const Order = () => {
-  return <div>order</div>;
+  const dispatch = useDispatch();
+
+  const proccedToPayment = () => {
+    dispatch(placeOrder({}));
+  };
+
+  return (
+    <div>
+      order
+      <button onClick={() => proccedToPayment()}>place Order</button>
+    </div>
+  );
 };
 
 export default Order;
