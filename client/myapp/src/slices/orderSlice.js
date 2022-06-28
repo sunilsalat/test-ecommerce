0838f52def0c319fd6b2c027952eeb65592d793e
sunilsalat <sunilsalat@yahoo.com>
2022-06-28 09:59:22 +0530

    new changes

--- client/myapp/src/slices/orderSlice.js
+++ client/myapp/src/slices/orderSlice.js
@@ -1,6 +1,22 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const placeOrder = createAsyncThunk();
+export const placeOrder = createAsyncThunk(
+  "order/placeOrder",
+  async ({}, { rejectWithValue, getState }) => {
+    try {
+      const { cartItems, address } = getState.cartItems;
+      const { paymentMethod } = getState.order;
+
+      const res = await fetch("/api/v1/order/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ cartItems, paymentMethod, address }),
+      });
+    } catch (error) {}
+  }
+);
 
 const orderSlice = createSlice({
   name: "order",
